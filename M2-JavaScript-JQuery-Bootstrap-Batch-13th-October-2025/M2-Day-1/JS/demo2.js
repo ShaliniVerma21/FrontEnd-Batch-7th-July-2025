13ca606dcfb700c3b954d09cd43c192db6c40f14
Shalini Verma <46881467+ShaliniVerma21@users.noreply.github.com>
2025-10-13 11:51:25 +0530

    Add files via upload

---
+++ M2-JavaScript-JQuery-Bootstrap-Batch-13th-October-2025/M2-Day-1/JS/demo2.js
@@ -0,0 +1,53 @@
+//single Line Comments
+
+/*
+MultiLine
+Comments
+*/
+
+//print message on console
+console.log("Hello, World!");
+console.log("<font color='purple'><br>Hello, World! welcome to Javascript Session-2</font>");
+
+//print message on webpage
+document.write("Hello, World! welcome to Javascript Session-2");
+
+//display text on webpage within colors
+document.write("<font color='red'><br>Hello, World! welcome to Javascript Session-2</font>");
+document.write("<font color='blue'><br>Hello, World! welcome to Javascript Session-2</font>");
+document.write("<font color='yellow'><br>Hello, World! welcome to Javascript Session-2</font>");
+document.write("<font color='green'><br>Hello, World! welcome to Javascript Session-2</font>");
+document.write("<font color='purple'><br>Hello, World! welcome to Javascript Session-2</font>");
+
+//display alert message
+alert("Hello, World! welcome to Javascript Session-2");
+window.alert("<font color='purple'><br>Hello, World! welcome to Javascript Session-2</font>");
+
+//what is variable?
+//variable is a container to store data that can be used later according to requirements.
+
+a=3;
+b=5;
+console.log(a+b); //output: 8
+
+Hello = "<font color='black'><br>Hello, World! welcome to Javascript Session-2</font>";
+document.write(Hello); //output:
+
+myName = "Shalini Verma";
+console.log(myName); //output: Shalini Verma
+
+//---------------------------- Self Work ------------------------------------
+
+//var (var is function scope variable,var is used for declaring variables in old JavaScript)
+var a = 10;
+
+//let, const (introduced in Javascript es6 version in 2015)
+//let and const are block scope variables
+//let and const are used for declaring variables in modern JavaScript
+let b = 20;
+const c = 30;
+
+
+//take input from user
+var name = prompt("Enter your name");
+console.log(name); //output: name entered by user
